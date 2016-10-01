c673b95201b4950b06d10da594e3ccdfbe3e1ad0
Andrew <zep519@yahoo.com>
2016-10-01 07:39:09 -0500

    First real commit.  Enum class.

---
+++ src/AB.Bootstrapper/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AB.Bootstrapper
+{
+    public class Enums
+    {
+        /// <summary>
+        /// Enumeration that defines the four major cardinal directions.
+        /// </summary>
+        public enum CardinalDirection
+        {
+            Unspecified = 0,
+            North = 1,
+            South = 2,
+            East = 3,
+            West = 4
+        }
+    }
+}
