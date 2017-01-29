3f8e37823d6d9a122c291e1bcf82f9d264aff740
Andrew <zep519@yahoo.com>
2017-01-29 15:17:47 -0600

    initial project for Arango DB Repo

---
+++ src/AB.Bootstrapper.Arango/ArangoGenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using AB.Bootstrapper.Interfaces.Repository;
+using ArangoDB.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AB.Bootstrapper.Arango
+{
+    public class ArangoGenericSingletonRepository<T,TId> : ITestSingletonRepository<T, TId>
+    {
+        private readonly string _connectionString;
+        private readonly ArangoDatabase _db;
+        public ArangoGenericSingletonRepository(string connectionString, string databaseName = null)
+        {
+            _connectionString = connectionString;
+            if (string.IsNullOrEmpty(databaseName)) databaseName = typeof(T).Name;
+            _db = new ArangoDatabase(connectionString, databaseName);
+        }
+
+        public Task<bool> DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteById(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindById(TId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ulong> GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AB.Bootstrapper/Interfaces/Repository/ITestSingletonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AB.Bootstrapper.Interfaces.Repository
+{
+    public interface ITestSingletonRepository<T, TId>
+    {
+        Task<T> FindById(TId id);
+        Task<IEnumerable<T>> ReadAll();
+        Task<ulong> GetCount();
+        Task<bool> DeleteById(TId id);
+        Task<bool> DeleteAll();
+    }
+}
