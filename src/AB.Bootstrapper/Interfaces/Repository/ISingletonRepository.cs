368932f299b6a7031550969ba44f9f2efee97909
Andrew <zep519@yahoo.com>
2017-01-29 14:31:49 -0600

    renaming IRepo to ITransientRepo.  I want separate designs for a singleton style repository and a transient version. Added ISingletonRepository

---
+++ src/AB.Bootstrapper/Interfaces/Repository/ISingletonRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AB.Bootstrapper.Interfaces.Repository
+{
+    /// <summary>
+    /// Common interface for my singleton repositories.  This design is a work in progress. 
+    /// *** All inherited repositories should adhere to CRUD naming conventions.  This is the most logical, and often specific 
+    /// database implementations should have class names following the template [Database][Domain]Repository.
+    /// Ex: SqlCustomerRepository
+    /// What can go here: Members that assist in understanding all transient repo interfaces, universally, regardless of database or data type.
+    /// </summary>
+
+    public interface ISingletonRepository
+    {
+    }
+}
--- src/AB.Bootstrapper/Interfaces/Repository/ITransientRepository.cs
+++ src/AB.Bootstrapper/Interfaces/Repository/ITransientRepository.cs
@@ -3,24 +3,24 @@
 namespace AB.Bootstrapper.Interfaces.Repository
 {
     /// <summary>
-    /// Common interface for all of my repository implementations.  This design works generally well for me.  I don't like DB specific
-    /// exceptions leaking up to my calling code, so my repo's tend to be transient in lifetime, and have state which is managed by the repo itself to
-    /// indicate status.
+    /// Common interface for my transient repositories.  This design works well for me.  I don't like DB specific
+    /// exceptions leaking up to my calling code, and these are generally small enough to even use in controller methods if needed.  
+    /// State which is managed by the repo itself to indicate status.
     /// *** All inherited repositories should adhere to CRUD naming conventions.  This is the most logical, and often specific 
     /// database implementations should have class names following the template [Database][Domain]Repository.
     /// Ex: SqlCustomerRepository
-    /// What can go here: Members that assist in understanding all repo interfaces, universally, regardless of database or data type.
+    /// What can go here: Members that assist in understanding all transient repo interfaces, universally, regardless of database or data type.
     /// </summary>
-    public interface IRepository
+    public interface ITransientRepository
     {
         Exception LastException { get; }
     }
 
     public enum RepositoryStatus
     {
-        Error = 0,
-        Success = 1,
-        Initialized = 2,
+        Initialized = 0,
+        Error = 1,
+        Success = 2,        
         DatabaseUnavailable = 3,
         DatabaseTimeout = 4,
         NoResultRecords = 5,
