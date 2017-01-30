f409234a9af9f0d86e98b7e3ef1766d44af39671
Andrew <zep519@yahoo.com>
2017-01-29 18:18:26 -0600

    basic implementations for Arango and logging statements

--- src/AB.Bootstrapper.Arango/ArangoGenericRepository.cs
+++ src/AB.Bootstrapper.Arango/ArangoGenericRepository.cs
@@ -1,5 +1,7 @@
 ﻿using AB.Bootstrapper.Interfaces.Repository;
 using ArangoDB.Client;
+using ArangoDB.Client.Collection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,40 +9,69 @@ using System.Threading.Tasks;
 
 namespace AB.Bootstrapper.Arango
 {
-    public class ArangoGenericSingletonRepository<T,TId> : ITestSingletonRepository<T, TId>
+    public class ArangoGenericSingletonRepository<T> : ITestSingletonRepository<T, string>
     {
-        private readonly string _connectionString;
-        private readonly ArangoDatabase _db;
-        public ArangoGenericSingletonRepository(string connectionString, string databaseName = null)
+        private readonly DatabaseSharedSetting _settings;
+        private readonly ILogger<ArangoGenericSingletonRepository<T>> _logger;
+        //Arango Domain class mappings: http://stackoverflow.com/questions/38844008/design-entities-with-union-architecture-concepts
+        public ArangoGenericSingletonRepository(DatabaseSharedSetting settings,ILogger<ArangoGenericSingletonRepository<T>> logger = null)
         {
-            _connectionString = connectionString;
-            if (string.IsNullOrEmpty(databaseName)) databaseName = typeof(T).Name;
-            _db = new ArangoDatabase(connectionString, databaseName);
+            if (logger != null) _logger = logger;
+            //if (string.IsNullOrEmpty(databaseName)) databaseName = typeof(T).Name;
+            _logger.LogDebug("{repository} successfully instantiated using {settings}.  Pointing Arango Repo to {url}",this,settings,settings.Url);
         }
 
-        public Task<bool> DeleteAll()
+        public async Task<bool> DeleteAll()
         {
-            throw new NotImplementedException();
+            using (ArangoDatabase db = new ArangoDatabase(_settings))
+            {
+                //this was grabbed from the docs and is not using the clean truncate way to do it, but I can't find truncate in the c# driver
+                //remove products transactionally and return removed results
+                IAqlModifiable<T> removedResults = db.Query<T>().Remove();
+                _logger.LogDebug("{repository} ran DeleteAll and returned {removedResults}",this,removedResults);
+                return true;
+            }
         }
 
-        public Task<bool> DeleteById(TId id)
+        public async Task<bool> DeleteById(string id)
         {
-            throw new NotImplementedException();
+            using (ArangoDatabase db = new ArangoDatabase(_settings))
+            {
+                IDocumentIdentifierResult removedResults = await db.Collection<T>().RemoveByIdAsync(id);
+                _logger.LogDebug("{repository} ran DeleteById and returned {removedResults}", this, removedResults);
+                return true;
+            }
         }
 
-        public Task<T> FindById(TId id)
+        public async Task<T> FindById(string id)
         {
-            throw new NotImplementedException();
+            using (ArangoDatabase db = new ArangoDatabase(_settings))
+            {
+                var obj = await db.DocumentAsync<T>(id);
+                _logger.LogDebug("{repository} ran FindById and returned {obj}", this, obj);
+                return obj;
+            }
         }
 
-        public Task<ulong> GetCount()
+        public async Task<ulong> GetCount()
         {
-            throw new NotImplementedException();
+            using (ArangoDatabase db = new ArangoDatabase(_settings))
+            {
+                long? count = db.Query<T>().Statistics.Count;
+                _logger.LogDebug("{repository} ran FindById and returned {count}", this, count);
+                if (count.HasValue) return (ulong)count.Value;
+                else return 0;
+            }
         }
 
-        public Task<IEnumerable<T>> ReadAll()
+        public async Task<IEnumerable<T>> ReadAll()
         {
-            throw new NotImplementedException();
+            using (ArangoDatabase db = new ArangoDatabase(_settings))
+            {
+                var all = await db.All<T>().ToListAsync();
+                _logger.LogDebug("{repository} ran ReadAll and returned {all}", this, all);
+                return all;
+            }
         }
     }
 }
